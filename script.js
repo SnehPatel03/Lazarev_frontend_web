ce202bb1633ecc66165f0018c5fd801ac0bc814a
PatelSnehV <snehpatel1233@gmail.com>
2025-07-22 10:09:01 +0530

    changed

--- script.js
+++ script.js
@@ -1,91 +1,112 @@
+// Animates the entire page load sequence
+function loadingAnimation() {
+  const tl = gsap.timeline();
+
+  tl.from("#page1", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 0.2,
+  })
+    .from("#page1", {
+      transform: "scaleX(0.7) scaleY(0.2)",
+      borderRadius: "150px",
+      duration: 2,
+      ease: "expo.out",
+    })
+    .from("nav", {
+      opacity: 0,
+      duration: 0.5,
+    })
+    .from("#page1 h1, #page1 p, #page1 div span h5", {
+      opacity: 0,
+      duration: 0.5,
+      stagger: 0.2,
+    });
+}
+
+// Animates the nav when hovered
 function navAnimation() {
-  var nav = document.querySelector("nav");
-  nav.addEventListener("mouseenter", function () {
-    var tl = gsap.timeline();
+  const nav = document.querySelector("nav");
+
+  nav.addEventListener("mouseenter", () => {
+    const tl = gsap.timeline();
+
     tl.to("#nav-bottom", {
       height: "20vh",
       bottom: "-20vh",
-      duration: 0.2,
-      delay: 0,
+      duration: 0.6,
+    }).from("#nav-bottom h5 span", {
+      // opacity: 0,
+      y: 20,
+      duration: 1,
+      stagger: { amount: 0.3 },
     });
-   
   });
-  nav.addEventListener("mouseleave", function () {
-    var tl = gsap.timeline();
-    tl.to("#nav-bottom", {
+
+  nav.addEventListener("mouseleave", () => {
+    gsap.to("#nav-bottom", {
       height: "0vh",
-      bottom: " 0vh",
+      bottom: "0vh",
       duration: 0.1,
     });
-    tl.from("#nav-bottom h5", {
-      opacity:0,
-      duration: 1.5,
-      stagger: {
-        amount: 0.3,
-      }
-    });
-    // tl.to("#nav-bottom h5", {
-    //   y: 20,
-    //   duration: 0.5,
-    //   stagger: {
-    //     amount: 0.3,
-    //   }
-    // });
   });
 }
-navAnimation();
+
+// Smaller sequential animations for page elements
 function smallanim() {
-  var tl = gsap.timeline();
+  const tl = gsap.timeline();
+
   tl.from("#page1 h5", {
-    y: -10,
+    y: -150,
     duration: 0.6,
-    stagger: 0.5,
-  });
-  tl.from("#page1 #head h1", {
-    y: -25,
-    duration: 0.6,
-    stagger: 1,
-  });
-  tl.from("#page1 #detail h4", {
-    y: 30,
-    duration: 0.7,
-    stagger: 0.5,
-  });
-  tl.from("#page1 #moving", {
-    y: 120,
-    duration: 0.4,
-    stagger: 0.5,
-  });
-  tl.from("#page2 video", {
-    y: 120,
-    duration: 1,
-    delay: 0.5,
-    stagger: 0.5,
-  });
+    stagger: 0.3,
+  })
+    .from("#page1 #head h1", {
+      y: -50,
+      duration: 0.6,
+      stagger: 0.3,
+    })
+    .from("#page1 #detail h4", {
+      y: 40,
+      duration: 0.7,
+      stagger: 0.3,
+    })
+    .from("#page1 #moving", {
+      y: 160,
+      duration: 0.4,
+    })
+    .from("#page2 video", {
+      y: 120,
+      duration: 1,
+      delay: 0.3,
+    });
 }
+
+loadingAnimation();
+navAnimation();
 smallanim();
+
 function crsr() {
   var video = document.querySelector("#page2 video");
   var crsr = document.querySelector("#crsr");
   video.addEventListener("mouseenter", function () {
-    gsap.to("#crsr" , {
-      opacity:1,
-      scale:1.
-    })
+    gsap.to("#crsr", {
+      opacity: 1,
+      scale: 1,
+    });
   });
   video.addEventListener("mousemove", function (dets) {
-    gsap.to("#crsr" , {
-      top:dets.y - 50,
-      left:dets.x -70
-    })
-  });  
-    video.addEventListener("mouseleave", function () {
-      gsap.to("#crsr" , {
-        opacity:0,
-        scale:0.
-      })
+    gsap.to("#crsr", {
+      top: dets.y - 50,
+      left: dets.x - 70,
+    });
   });
-  
+  video.addEventListener("mouseleave", function () {
+    gsap.to("#crsr", {
+      opacity: 0,
+      scale: 0,
+    });
+  });
 }
 crsr();
 function rightelemAnimation() {
@@ -183,7 +204,9 @@ sections.forEach(function (elem) {
   });
   elem.addEventListener("mouseleave", function () {
     elem.childNodes[3].style.opacity = 0;
-    elem.childNodes[3].load()
-})
+    elem.childNodes[3].load();
+  });
 });
+
+navAnimation();
 
